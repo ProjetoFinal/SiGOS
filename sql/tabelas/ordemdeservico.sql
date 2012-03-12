0028912f1bce9927fdd5bcb3e4231c10bf9b00b9
Leonardo Neves <leo.mvhost@hotmail.com>
2012-03-12 15:04:36 -0300

    alteracoes idfuncionario para idusuario

--- sql/tabelas/ordemdeservico.sql
+++ sql/tabelas/ordemdeservico.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.2deb1ubuntu1
+-- version 3.3.7deb5build0.10.10.1
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: Mar 10, 2012 as 07:16 PM
--- Versão do Servidor: 5.1.41
--- Versão do PHP: 5.3.2-1ubuntu4.14
+-- Tempo de Geração: Mar 12, 2012 as 03:03 PM
+-- Versão do Servidor: 5.1.49
+-- Versão do PHP: 5.3.3-1ubuntu9.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -28,7 +28,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `ordemdeservico` (
   `idordemdeservico` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `idequipamento` int(11) unsigned NOT NULL,
-  `idfuncionario` int(11) DEFAULT NULL,
+  `idusuario` int(11) DEFAULT NULL,
   `idstatus` int(11) NOT NULL,
   `defeito` text NOT NULL,
   `acompanhamento` text,
@@ -42,12 +42,12 @@ CREATE TABLE IF NOT EXISTS `ordemdeservico` (
   `caminhoimpressao` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`idordemdeservico`),
   KEY `idEquipamento` (`idequipamento`),
-  KEY `idFuncionario` (`idfuncionario`)
+  KEY `idFuncionario` (`idusuario`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=2 ;
 
 --
 -- Extraindo dados da tabela `ordemdeservico`
 --
 
-INSERT INTO `ordemdeservico` (`idordemdeservico`, `idequipamento`, `idfuncionario`, `idstatus`, `defeito`, `acompanhamento`, `acessorios`, `solucao`, `entrada`, `iniciomanut`, `fimmanut`, `entrega`, `garantiadeservico`, `caminhoimpressao`) VALUES
+INSERT INTO `ordemdeservico` (`idordemdeservico`, `idequipamento`, `idusuario`, `idstatus`, `defeito`, `acompanhamento`, `acessorios`, `solucao`, `entrada`, `iniciomanut`, `fimmanut`, `entrega`, `garantiadeservico`, `caminhoimpressao`) VALUES
 (1, 6, NULL, 1, 'Tv não liga', NULL, 'Controle', NULL, '2012-03-10', NULL, NULL, NULL, NULL, 'impressao/os1331417715.txt');
