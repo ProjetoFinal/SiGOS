c19d2d4c2d207ebca88ab511044e8aebd65b199f
nadoneves <leo.mvhost@hotmail.com>
2012-05-18 16:20:07 -0300

    layout relatorio.php

--- template/gerente/relatorio.php
+++ template/gerente/relatorio.php
@@ -9,20 +9,21 @@ include_once("../function/formataData.php");
 
 ?>
 
-<br /><span
+<br /><br /><span
 	style="
 		font-size: 20px;
 		font-weight: bold;
 		color: #777;
 	";>
-	Selecione apenas um critério para consulta
+	Relatórios
 </span>
 
-<form id="form1" method="post" action="ajax/gerarRelatorio.php">
-<div id="column1">
-<table>
-	<tr>
-		<td>Relatório de OS por Status: </td>
+
+<div id="column1" style="border:0px solid red; margin: 50px 0 0 80px">
+<table border=0>
+
+	<tr><form id="form1" method="post" action="ajax/gerarRelatorio.php">
+		<td>Relatório de OS por Status</td>
 		<td>
 			<select name="OSPorStatus" id="OSPorStatus">
 				<option value="" selected>-- STATUS</option>
@@ -36,54 +37,88 @@ include_once("../function/formataData.php");
 				<?php } ?>
 			</select>
 		</td>
-	</tr>
-	<tr>
-		<td>Relatório de OS por período: </td>
-		<td>
+		<td align="right">
+			<div style="margin-top:-10px">&nbsp;&nbsp;
+				<input type="submit" class="bt_gerarrel" id="gerarRelatorio" onclick="loading()" value="Gerar Relatório (Ctrl + F11)" />
+				<input type="reset" class="bt_limpar" id="cancelar" value="Cancelar (F5)" />
+			</div>
+		</td>
+	</tr></form>
+
+	<tr><td colspan="4">&nbsp;</td></tr>
+	<tr><td colspan="4"><div style="border-top:3px solid #aaa ">&nbsp;</div></tr>
+
+	<tr><form id="form1" method="post" action="ajax/gerarRelatorio.php">
+		<td>Relatório de OS por período</td>
+		<td align="left">
 			<input type="text" name="OSPorPeriodoInicial" id="OSPorPeriodoInicial"
 				style="width:150px !important;
-					   margin-right: 5px!important" readonly />
+					   margin-right: 5px!important" readonly />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;à
 		</td>
 		<td>
 			<input type="text" name="OSPorPeriodoFinal" id="OSPorPeriodoFinal"
 				style="width:150px !important;
 					   margin-right: 5px!important" readonly />
 		</td>
-	</tr>
-	<tr>
-		<td>Relatório de Compras por período: </td>
 		<td>
+			<div style="margin-top:-10px">
+				<input type="submit" class="bt_gerarrel" id="gerarRelatorio" onclick="loading()" value="Gerar Relatório (Ctrl + F11)" />
+				<input type="reset" class="bt_limpar" id="cancelar" value="Cancelar (F5)" />
+			</div>
+		</td>
+	</tr></form>
+
+	<tr><td colspan="4">&nbsp;</td></tr>
+	<tr><td colspan="4"><div style="border-top:3px solid #aaa ">&nbsp;</div></tr>
+
+	<tr><form id="form1" method="post" action="ajax/gerarRelatorio.php">
+		<td>Relatório de Compras por período</td>
+		<td align="left">
 			<input type="text" name="ComprasPorPeriodoInicial" id="ComprasPorPeriodoInicial"
 				style="width:150px !important;
-					   margin-right: 5px!important" readonly />
+					   margin-right: 5px!important" readonly />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;à
 		</td>
 		<td>
 			<input type="text" name="ComprasPorPeriodoFinal" id="ComprasPorPeriodoFinal"
 				style="width:150px !important;
 					   margin-right: 5px!important" readonly />
 		</td>
-	</tr>
-	<tr>
-		<td>Relatório de Faturamento por período: </td>
 		<td>
+			<div style="margin-top:-10px">
+				<input type="submit" class="bt_gerarrel" id="gerarRelatorio" onclick="loading()" value="Gerar Relatório (Ctrl + F11)" />
+				<input type="reset" class="bt_limpar" id="cancelar" value="Cancelar (F5)" />
+			</div>
+		</td>
+	</tr></form>
+
+	<tr><td colspan="4">&nbsp;</td></tr>
+	<tr><td colspan="4"><div style="border-top:3px solid #aaa ">&nbsp;</div></tr>
+
+	<tr><form id="form1" method="post" action="ajax/gerarRelatorio.php">
+		<td>Relatório de Faturamento por período</td>
+		<td align="left">
 			<input type="text" name="FaturamentoPorPeriodoInicial" id="FaturamentoPorPeriodoInicial"
 				style="width:150px !important;
-					   margin-right: 5px!important" readonly />
+					   margin-right: 5px!important" readonly />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;à
 		</td>
 		<td>
 			<input type="text" name="FaturamentoPorPeriodoFinal" id="FaturamentoPorPeriodoFinal"
 				style="width:150px !important;
 					   margin-right: 5px!important" readonly />
 		</td>
-	</tr>
+		<td>
+			<div style="margin-top:-10px">
+				<input type="submit" class="bt_gerarrel" id="gerarRelatorio" onclick="loading()" value="Gerar Relatório (Ctrl + F11)" />
+				<input type="reset" class="bt_limpar" id="cancelar" value="Cancelar (F5)" />
+			</div>
+		</td>
+	</tr></form>
+
 </table>
 </div>
 
-<div id="lineButton">
-	<input type="button" class="bt_gerarrel" id="gerarRelatorio" value="Gerar Relatório (Ctrl + F11)" />
-	<input type="button" class="bt_limpar" id="cancelar" value="Cancelar (F5)" />
-</div>
-</form>
+
+
 
 <div id="retornoErro"></div>
 <div id="retorno"></div>
@@ -131,6 +166,7 @@ include_once("../function/formataData.php");
 	    $("#cancelar").click( function(){
 	    	$(window.document.location).attr('href','relatorio.php');
 	    });
+	    /*
 	    $("#gerarRelatorio").click( function(){
 	    	var formulario = $("#form1").serialize();
 	    	$.ajax({
@@ -148,5 +184,12 @@ include_once("../function/formataData.php");
 	            }
 	    	});
 	    });
+		*/
 	});
+
+	function loading(){
+		$('#retornoErro').fadeIn(1000);
+        $("#retornoErro").text('Gerando Relatório...');
+        $("#retornoErro").fadeOut(1000);
+	}
 </script>
